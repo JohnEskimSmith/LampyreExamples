44bfe2cba4e8024a35e3c0d7ef125bdcf054082b
Andrey I. Skhomenko <a.skhomenko@norsi-trans.ru>
2019-10-10 21:59:15 +0300

    add Schema to MAP add Schema for Graph add some icon add select lang(ru, en)

--- Googles/search_place_by_google.py
+++ Googles/search_place_by_google.py
@@ -18,11 +18,14 @@ def key_textsearch():
 
 try:
     from ontology import (
-        Task, Header, HeaderCollection, Utils, Field, ValueType, SchemaLink, SchemaObject, Condition, Operations, Macro,
+        Task, Header, Location, Object, Link, Attribute, HeaderCollection, Utils, Field, ValueType, SchemaLink, SchemaObject, Condition, Operations, Macro,
         MacroCollection, Schema, EnterParamCollection, SchemaCollection, GraphMappingFlags, BinaryType, Constants,
         Attributes)
 except:
     pass
+
+def not_empty(field: Field):
+    return Condition(field, Operations.NotEqual, '')
 
 
 def return_dummy(row):
@@ -31,7 +34,7 @@ def return_dummy(row):
     return hash(_line)
 
 
-def text_search_google_place(text, api, logger, length_geo=6, delay=5):
+def text_search_google_place(text, api, logger, length_geo, lang, delay=5):
 
     def download_photo_base64(url):
         '''
@@ -77,6 +80,8 @@ def text_search_google_place(text, api, logger, length_geo=6, delay=5):
                 sleep_before_geturl(3)
 
             params['key'] = api
+            params['language'] = lang
+            params['region'] = lang.upper()
             page = session_request.get(get_url_text, params=params, timeout=delay)
             jsonRaw = page.json()
             status_code = page.status_code
@@ -124,6 +129,7 @@ def text_search_google_place(text, api, logger, length_geo=6, delay=5):
                     _tmp['lat'] = row['geometry']['location']['lat']
                     _tmp['lon'] = row['geometry']['location']['lng']
                     _tmp['geohash'] = pgh.encode(_tmp['lat'],_tmp['lon'], precision=length_geo)
+                    _tmp['geohash_size'] = length_geo
                     _tmp['search_text'] = text
                     if 'photos' not in row:
                         _tmp['photo_data'] = ''
@@ -144,11 +150,11 @@ def text_search_google_place(text, api, logger, length_geo=6, delay=5):
             return _return_results
 
 
-def thread_async_(texts, threads, length_geo, logger):
+def thread_async_(texts, threads, length_geo, lang, logger):
     google_key = key_textsearch()
     result_rows = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        future_rows = {executor.submit(text_search_google_place, text, google_key, logger, length_geo):
+        future_rows = {executor.submit(text_search_google_place, text, google_key, logger, length_geo, lang):
                            text for text in texts}
         for future in concurrent.futures.as_completed(future_rows):
             try:
@@ -171,7 +177,53 @@ class GoogleTextSearchPlaces(metaclass=Header):
     lat = Field('lat', ValueType.Float)
     lon = Field('lon', ValueType.Float)
     geohash = Field('geohash', ValueType.String)
+    geohash_size = Field('geohash_size', ValueType.Integer)
 
+class SchemaLocation(metaclass=Schema):
+    name = 'Google: Locations'
+    Header = GoogleTextSearchPlaces
+
+    SchemaLocationObject = SchemaObject(Location, mapping={Location.Name: Header.formatted_address,
+                                                           Location.Address: Header.name_address,
+                                                           Location.Latitude: Header.lat,
+                                                           Location.Longitude: Header.lon,
+                                                           Location.Geohash: Header.geohash,
+                                                           Location.GeoPoint:[Header.lat, Header.lon]})
+
+class GeoHash(metaclass=Object):
+    name = "Geohash"
+    geohash = Attribute("Geohash value", ValueType.String)
+    geohash_size = Attribute("Geohash size", ValueType.Integer)
+    IdentAttrs = [geohash]
+    CaptionAttrs = [geohash, geohash_size]
+    Image = Utils.base64string(r"C:\LampyreExamples\Googles\geohash_icon.png")
+
+
+class LocationToGeohash(metaclass=Link):
+    name = Utils.make_link_name(Location, GeoHash)
+    Desc = Attributes.System.Text
+    CaptionAttrs = [Desc]
+    Begin = Location
+    End = GeoHash
+
+
+class SchemaLocationGeohash(metaclass=Schema):
+    name = 'Schema: Locations and Geohash'
+    Header = GoogleTextSearchPlaces
+
+    SchemaLocationObject = SchemaObject(Location, mapping={Location.Name: Header.formatted_address,
+                                                           Location.Address: Header.name_address,
+                                                           Location.Latitude: Header.lat,
+                                                           Location.Longitude: Header.lon,
+                                                           Location.Geohash: Header.geohash,
+                                                           Location.GeoPoint:[Header.lat, Header.lon]})
+
+    SchemaGeohash = SchemaObject(GeoHash, mapping={GeoHash.geohash: Header.geohash,
+                                                           GeoHash.geohash_size: Header.geohash_size})
+
+    SchemaLocationToGeohash = LocationToGeohash.between(SchemaLocationObject, SchemaGeohash,
+                                                        mapping={LocationToGeohash.Desc: Header.name_address},
+                                                        conditions=[not_empty(Header.name_address)])
 
 class GoogleSearchTextPlace(Task):
 
@@ -180,7 +232,7 @@ class GoogleSearchTextPlace(Task):
         self.info, self.error, self.result, self.api, self.api_key = [None] * 5
 
     def get_id(self):
-        return '41b49f4a-64c1-4b4f-84e3-73448e81953d'
+        return 'ac2b9534-8e51-4abe-84cf-74658ecdc945'
 
     def get_display_name(self):
         return 'API Google:Places'
@@ -195,11 +247,15 @@ class GoogleSearchTextPlace(Task):
         htable.set_property(htable.lon.system_name, 'longitude', True)
         return HeaderCollection(htable)
 
+    def get_schemas(self):
+        return SchemaCollection(SchemaLocation, SchemaLocationGeohash)
 
     def get_enter_params(self):
         ep_coll = EnterParamCollection()
         ep_coll.add_enter_param('texts', 'Search places', ValueType.String, is_array=True,
                                 required=True)
+        ep_coll.add_enter_param('language', 'Language', ValueType.String, is_array=False,
+                                required=True, default_value='ru', predefined_values=['ru', 'en'])
         ep_coll.add_enter_param('length_geo', 'geohash', ValueType.Integer, required=True, default_value=6)
         return ep_coll
 
@@ -207,7 +263,19 @@ class GoogleSearchTextPlace(Task):
         log_writer.info(enter_params)
         _texts = list(map(lambda z: z.strip(), enter_params.texts))
         maxthreads = 6
-        _rows = thread_async_(_texts, maxthreads, enter_params.length_geo, log_writer)
+        try:
+            geohash = int(enter_params.length_geo)
+        except:
+            geohash = 6
+        if geohash > 14:
+            log_writer.info('set geohash: 6')
+
+        lang = enter_params.language
+        if lang not in ['en', 'ru']:
+            log_writer.info('set language:"en"')
+            lang = 'en'
+
+        _rows = thread_async_(_texts, maxthreads, geohash, lang, log_writer)
         rows = []
         _list_of_hash = []
         for line in _rows:
@@ -231,8 +299,9 @@ if __name__ == '__main__':
     DEBUG = True
 
     class EnterParamsFake:
-        texts = ['United Nations USA']
+        texts = ['offices Google France']
         length_geo = 6
+        language = 'ru'
 
 
     class WriterFake:
